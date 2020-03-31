88f3b5aa29594d701cd7ec8dfc846e6d676d8c8d
Asilver-jpg <asilver7493@gmail.com>
2020-03-31 18:23:55 -0400

    Done.

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ module FormsLab
 
     # code other routes/actions here
     get '/' do 
-      erb :new
+      erb :"pirates/new"
     end
 
     post '/pirates' do
@@ -17,7 +17,7 @@ module FormsLab
 
         Ship.new(ship["type"], ship["booty"])
       end
-      erb :show
+      erb :"pirates/show"
       end
     end
   end
